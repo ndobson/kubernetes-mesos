8db45ae3d52d5c3814458cfb74b91dd24c39e8db
Nick Dobson <nicholas.dobson@ge.com>
2015-08-14 14:34:02 -0500

    Docker 1.0 cookbook changes

--- recipes/etcd.rb
+++ recipes/etcd.rb
@@ -4,10 +4,10 @@ end
 
 # Launch etcd
 docker_container 'etcd' do
-  detach true
-  hostname node['hostname']
-  image node['kubernetes-mesos']['etcd']['image_name']
+  host_name node['hostname']
+  repo node['kubernetes-mesos']['etcd']['image_name']
   tag node['kubernetes-mesos']['etcd']['image_tag']
-  port ['4001:4001', '7001:7001']
+  port '4001:4001, 7001:7001'
   command "--listen-client-urls http://0.0.0.0:4001 --advertise-client-urls http://#{node['ipaddress']}:4001"
+  restart_policy 'always'
 end
